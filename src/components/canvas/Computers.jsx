6a6c29988422bef718f869616cbc73487d0d68ad
JordiMartinez <jordiupc87@gmail.com>
2023-05-26 18:06:56 +0200

    3D desktop visible, not finished/positioned correctly

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -8,8 +8,42 @@ const Computers = () => {
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black" />
+      <pointLight intensity={1} />
+      <spotLight position={[-20, 50, 10]}></spotLight>
+      <primitive
+        object={computer.scene}
+        scale={2}
+        position={[0, 0, 0]}
+        rotation={[-0.01, -0.2, -0.1]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
     </mesh>
   );
 };
 
-export default Computers;
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [-20, 50, -0], fav: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas;
